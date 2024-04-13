e0688f14e7ff19c41708f1aab72830fb471d7a95
Yoni Klain <yoni.klain@gmail.com>
2024-04-13 20:07:23 +0300

    options styling

--- components/options.tsx
+++ components/options.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react';
+import { useAIState } from 'ai/rsc'
 
 type OptionButtonProps = {
   option: string;
@@ -8,9 +9,9 @@ type OptionButtonProps = {
 
 const Options = (props: OptionButtonProps) => {
   return (
-    <div>
+    <div className="text-left my-4" >
       {/* Correct the onClick handler to call onSubmit with the option */}
-      <button onClick={() => props.onSubmit(props.option)}>{props.option}</button>
+      <button className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='button' onClick={() => props.onSubmit(props.option)}>{props.option}</button>
     </div>
   )
 }
