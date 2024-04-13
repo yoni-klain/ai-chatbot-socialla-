3a6b4947b48c44b3a1f084afdd349b343897dd5e
Yoni Klain <yoni.klain@gmail.com>
2024-04-13 12:36:25 +0300

    Changed AI funcs desctiptions

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -21,7 +21,7 @@ import { z } from 'zod'
 
 import { Events } from '@/components/stocks/event'
 import { Stocks } from '@/components/stocks/stocks'
-import { loadVideosWithCaptions } from '../api/loadVideosWithCaptions';
+import { loadVideosWithCaptions } from '../api/loadVideosWithCaptions'
 import { Videos } from '@/components/videos/videos'
 import {
   formatNumber,
@@ -33,14 +33,12 @@ import { saveChat } from '@/app/actions'
 import { SpinnerMessage, UserMessage } from '@/components/stocks/message'
 import { Chat } from '@/lib/types'
 import { auth } from '@/auth'
-import VideoPlayer from "@/components/ui/youtube/video-player";
-import {VideoData} from "@/components/ui/youtube/types";
-
+import VideoPlayer from '@/components/ui/youtube/video-player'
+import { VideoData } from '@/components/ui/youtube/types'
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY || ''
 })
-
 
 async function submitUserMessage(content: string) {
   'use server'
@@ -69,19 +67,12 @@ async function submitUserMessage(content: string) {
     messages: [
       {
         role: 'system',
-        content: `\
-        Introduce yourself as Khilo, a video MVP important moments search bot.
-        Ask the user about their hobbies and suggest searching for video moments related to their interests.
-        Engage in a conversation to determine the specific search term key for videos.
-        Try to get specific search term by suggest specific topics to exploge in numeric list format.   
-        Call \`provide_video_captions_to_ai\` to retrieve video_id and timed captions in XML format.
-        Analyze the video captions to find the most relevant parts based on user requirements.
-        Automatically call \`show_video_moments\` to present the best moments to the user.
-        Refine the search term based on user feedback and call \`provide_video_captions_to_ai\` again for updated captions.
-        Adjust the video moments by updating start and end times if needed and then call \`show_video_moments\` to present the results.
-        Maintain a friendly tone and provide clarifications as necessary during the conversation with the user.
-        
-        Feel free to customize and expand on these instructions based on your specific requirements and functionalities.`
+        content: `
+        Hello, I'm Khilo, your video MVP search assistant. I can help you find important moments in videos based on your interests. 
+        Tell me about your hobbies, and let's find some engaging video moments together. 
+        Could you specify a topic or an event that interests you? Once I have that, I'll fetch some video captions and analyze them to find key moments that you'll find fascinating. 
+        Let's get started by discussing what you're interested in!
+      `
       },
       ...aiState.get().messages.map((message: any) => ({
         role: message.role,
@@ -116,26 +107,24 @@ async function submitUserMessage(content: string) {
     },
     functions: {
       provide_video_captions_to_ai: {
-        description: 'API to get 5 videos with captions for futher analysis based on captions',
+        description:
+          'Retrieves video data including captions for a specified search term. This API is optimized to fetch a limited set of videos (up to 5) to ensure focused analysis on highly relevant content. The retrieved captions are intended for subsequent processing to identify key moments that are most significant to the user’s interests.',
         parameters: z.object({
           searchKey: z
             .string()
-            .describe(
-              'The search term to search a video on youtube'
-            )
+            .describe('The YouTube search term used to find relevant videos.')
         }),
         render: async function* ({ searchKey }) {
           yield (
             <BotCard>
-              <div>`I am presenting videos`{ searchKey }</div>
+              <div>`I am presenting videos`{searchKey}</div>
             </BotCard>
           )
-          
-          
+
           await sleep(1000)
-          
-          const videos = await loadVideosWithCaptions(searchKey);
-          
+
+          const videos = await loadVideosWithCaptions(searchKey)
+
           aiState.done({
             ...aiState.get(),
             messages: [
@@ -149,51 +138,47 @@ async function submitUserMessage(content: string) {
             ]
           })
 
-          
           return (
             <BotCard>
-              <div>
-                Video moments comming soon..... 
-              </div>
+              <div>Video moments comming soon.....</div>
             </BotCard>
-          ) 
+          )
         }
       },
       show_video_moments: {
-        description: `Get list of video information including thumbnails url value and the start and end seconds of the best moments according to the user question at least 10 seconds length . 
-                      Limit to 5 best. 
-                      Use this to show the  start and the end of the best moments to the user.`,
+        description: `Presents the most relevant video segments as determined by AI analysis. This function displays up to five of the best moments, each annotated with start and end timestamps to guide the user directly to the highlights. Thumbs are provided to give a visual preview of each moment.`,
         parameters: z.object({
-          videoInformation: z.array(z.object({
-            videoId: z.string(),
-            videoTitle: z.string(),
-            bestMomentStart:z.number(),
-            bestMomentEnd:z.number(),
-            thumbnails: z.object({
+          videoInformation: z.array(
+            z.object({
+              videoId: z.string(),
+              videoTitle: z.string(),
+              bestMomentStart: z.number(),
+              bestMomentEnd: z.number(),
+              thumbnails: z.object({
                 default: z.object({
-                    url: z.string()
+                  url: z.string()
                 })
-                })
-          }))
+              })
+            })
+          )
         }),
         render: async function* ({ videoInformation }) {
-          const videoData:  VideoData[] = videoInformation.map(video => ({
+          const videoData: VideoData[] = videoInformation.map(video => ({
             start: video.bestMomentStart,
             end: video.bestMomentEnd,
             title: video.videoTitle,
             thumbnailUrl: video.thumbnails.default.url,
             videoId: video.videoId
-          }));
+          }))
 
           yield (
             <BotCard>
-              <div>
-                just before presenting results
-              </div>
+              <div>just before presenting results</div>
             </BotCard>
           )
-          
+
           await sleep(1000)
+          
           aiState.done({
             ...aiState.get(),
             messages: [
